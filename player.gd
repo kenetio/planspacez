4f4a9940610370e6ecfa49a070586907d9e1a53f
kenetio <danyakhairytdinov@gmail.com>
2025-03-29 10:21:07 +0300

    чв

--- player.gd
+++ player.gd
@@ -7,13 +7,16 @@ var speed = 500
 var speedr = 10
 var target_pos = Vector2()
 var new_transform = Vector2()
+var strel_transform = Vector2()
 var canshoot = true
 var lives = 100
 var quest = "none"
+
 func _physics_process(delta):
 	target_pos = get_global_mouse_position()
 	new_transform = transform.looking_at(target_pos)
 	transform  = transform.interpolate_with(new_transform, speedr * delta)
+	$Marker2D2.look_at(Vector2(266, 1401))
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 	$"../CanvasLayer/ProgressBar".value = lives
