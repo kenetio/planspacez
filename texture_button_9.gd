379c7944f5385889ba300a3ed18ae46c02caa9d6
Scvairel <ishigamisency@gmail.com>
2025-03-29 10:21:02 +0300

    Nastroiki
    ghj

---
+++ texture_button_9.gd
@@ -0,0 +1,17 @@
+extends TextureButton
+
+
+var gida = true
+
+
+
+func _on_pressed() -> void:
+	if gida == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		ProjectSettings.set("display/window/size/viewport_width", 1680)
+		ProjectSettings.set("display/window/size/viewport_height", 900)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		ProjectSettings.set("display/window/size/viewport_width", 1920)
+		ProjectSettings.set("display/window/size/viewport_height", 1080)
+		
